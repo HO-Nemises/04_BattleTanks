83e6a54149265b00f84114466b6601bff04b4664
Nem-win7\CraigR <ho.nemises@gmail.com>
2016-12-11 23:53:44 +0000

    Fixed previous bug with casting non pawn in TankAIController class

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -10,26 +10,27 @@ void ATankAIController::BeginPlay()
 	auto CurrentControlledTank = GetControlledTank();
 	auto PlayerControlledTank = GetPlayertank();
 
-//TODO : This is broken, I think get rid of GetControlledTank all together, not needed...should just log out PlayerControlledTank
-	if (!PlayerControlledTank || !CurrentControlledTank)
+
+	if (!PlayerControlledTank  || !CurrentControlledTank)
 	{
 		UE_LOG(LogTemp, Error, TEXT("cannot find player controlled Tank or AI tank not possesed"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AI %s found playercontroller %s"), *(CurrentControlledTank->GetName()), *(PlayerControlledTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("AI %s found playercontroller %s"),*(CurrentControlledTank->GetName()), *(PlayerControlledTank->GetName()));
 	}
 
 }
 
 ATank* ATankAIController::GetPlayertank() const
 {
-	return Cast<ATank>(GetWorld()->GetFirstPlayerController());
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
 
 }
+
 
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 
-}
+} 
