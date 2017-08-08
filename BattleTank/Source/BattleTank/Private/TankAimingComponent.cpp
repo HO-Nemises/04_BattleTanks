3907f44134e40386817acf7c9f7b8ec7cd34f3c4
Nem-win7\CraigR <ho.nemises@gmail.com>
2017-08-08 23:57:13 +0100

    no message

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -23,23 +23,7 @@ void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent * BarrelToSet
 
 }
 
-// Called when the game starts
-void UTankAimingComponent::BeginPlay()
-{
-	Super::BeginPlay();
 
-	// ...
-	
-}
-
-
-// Called every frame
-void UTankAimingComponent::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
-{
-	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
-
-	// ...
-}
 
 void UTankAimingComponent::AimAt(FVector HitLocation,float LaunchSpeed)
 {
@@ -57,9 +41,22 @@ void UTankAimingComponent::AimAt(FVector HitLocation,float LaunchSpeed)
 	if (UGameplayStatics::SuggestProjectileVelocity(this, OutLaunchVelocity, StartLocation, HitLocation, LaunchSpeed, false,0 ,0 ,ESuggestProjVelocityTraceOption::DoNotTrace))
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
-		auto TankName = GetOwner()->GetName();
-		UE_LOG(LogTemp, Warning, TEXT("%s aiming at at %s"),*TankName, *AimDirection.ToString());
+		//auto TankName = GetOwner()->GetName();
+		//UE_LOG(LogTemp, Warning, TEXT("%s aiming at at %s"),*TankName, *AimDirection.ToString());
+		MoveBarrelTowards(AimDirection);
+
 	}
 	//else do nothing
 }
 
+void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection) 
+{
+	auto BarrelRotation = Barrel->GetForwardVector().Rotation();
+	auto AimAsRotator = AimDirection.Rotation();
+	auto Deltarotator = AimAsRotator - BarrelRotation;
+
+	UE_LOG(LogTemp, Warning, TEXT("aimasrotator %s"), *AimAsRotator.ToString());
+
+	// Move the barrel the right amount this frame
+	// Given a max elevation speed and the frame time
+}
